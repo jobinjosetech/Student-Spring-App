94e974a481b08ac9aee2855b34a32e5f9b2fed80
jobin <jobinjose.in@gmail.com>
2022-12-05 14:37:25 +0530

    contact page

--- src/main/java/com/example/studentappnew_backend/controller/StudentController.java
+++ src/main/java/com/example/studentappnew_backend/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.studentappnew_backend.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,11 @@ public class StudentController {
     @GetMapping("/")
     public String HomePage(){
         return "Welcome to Spring App";
+    }
+
+    @GetMapping("/contact")
+    public String ContactPage(){
+        return "Welcome to contact page";
     }
 
 }
